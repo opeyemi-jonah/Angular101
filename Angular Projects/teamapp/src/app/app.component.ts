d13f1e54e04418e02476360b0a46ab4a3e2f0e62
Opeyemi G. Jonah <opeyemi.jonah@cygrpdal.local>
2022-09-13 23:11:33 -0600

    initial commit

---
+++ Angular Projects/teamapp/src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  newMemberName = '';
+  members: string[] = [];
+  errorMessage = "";
+
+  onInput(member: string){
+    this.newMemberName = member;
+    
+  }
+
+  addMember(){
+    if(!this.newMemberName){
+      this.errorMessage = "Name can not be empty";
+      return
+    }
+
+    this.members.push(this.newMemberName);
+    this.newMemberName = "";
+    
+  }
+
+
+}
