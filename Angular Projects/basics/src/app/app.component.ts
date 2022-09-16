8ee1acfc435022cacd8a6c0c7b78380040e796d7
opeyemi-jonah <gavrieljonah@gmail.com>
2022-09-15 23:01:41 -0600

    added ngClass to button in template

--- Angular Projects/basics/src/app/app.component.ts
+++ Angular Projects/basics/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { concat } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -11,13 +12,16 @@ export class AppComponent {
   age: number = 21;
   imgURL = "https://picsum.photos/id/1/200/300";
   imgURLTwo = 'https://i.picsum.photos/id/1003/1181/1772.jpg?hmac=oN9fHMXiqe9Zq2RM6XT-RVZkojgPnECWwyEF1RvvTZk';
+  currentDate = new Date();
+  blueClass = false;
+
 
   getName() : string {
     return this.name.concat(' jonah');
   }
 
   getAge(): string {
-    return this.age.toString();
+    return this.age.toString().concat(' years old');
   }
   changeImage(e:KeyboardEvent){
     
