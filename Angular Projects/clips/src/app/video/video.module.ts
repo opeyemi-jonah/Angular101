c73a138275d939e74afb5a7402d3be024ffea285
Opeyemi Jonah <gavrieljonah@gmail.com>
2023-01-03 13:52:39 -0700

    Added feature: File uploads to Firebase

--- Angular Projects/clips/src/app/video/video.module.ts
+++ Angular Projects/clips/src/app/video/video.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { VideoRoutingModule } from './video-routing.module';
 import { ManageComponent } from './manage/manage.component';
 import { UploadComponent } from './upload/upload.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 
@@ -14,7 +15,8 @@ import { UploadComponent } from './upload/upload.component';
   ],
   imports: [
     CommonModule,
-    VideoRoutingModule
+    VideoRoutingModule,
+    SharedModule
   ]
 })
 export class VideoModule { }
